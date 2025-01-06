df1da96e9c321ae970942da3bb8d4e7ef273fa1d
Ali Murtaza <iamalimurtazaa@gmail.com>
2025-01-06 22:48:16 +0500

    completed our mission & why choose us sections

--- js/app.js
+++ js/app.js
@@ -1,4 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Initiate Lucide Icons
-  // lucide.createIcons();
+  /**
+   * How it works
+   */
+  const tabItems = document.querySelectorAll(".tabs__item");
+  const imageWrappers = document.querySelectorAll(".image-wrapper");
+
+  // Show first image wrapper by default
+  imageWrappers[0].classList.add("active");
+
+  tabItems.forEach((tab) => {
+    tab.addEventListener("click", () => {
+      // Remove active class from all tabs
+      tabItems.forEach((item) => item.classList.remove("active"));
+
+      // Add active class to clicked tab
+      tab.classList.add("active");
+
+      // Get the tab number from data-tab attribute
+      const tabNumber = tab.getAttribute("data-tab");
+
+      // Remove active class from all image wrappers
+      imageWrappers.forEach((wrapper) => {
+        wrapper.classList.remove("active");
+      });
+
+      // Add active class to corresponding image wrapper
+      const activeImage = document.querySelector(
+        `.image-wrapper[data-tab="${tabNumber}"]`
+      );
+      activeImage.classList.add("active");
+    });
+  });
+
+  // next code
 });
